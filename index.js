e9270334125ce96c5a41f7d697164f516747cf38
Adam Kelsey <adam.kelsey113@gmail.com>
2016-08-23 16:09:07 -0400

    server set up and working, displaying user list

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var app = express()
+
+var fs = require('fs')
+var _ = require('lodash')
+var users = []
+
+fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
+  if (err) throw err
+
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    users.push(user)
+  })
+
+})
+
+app.get('/', function (req, res) {
+  var buffer = ''
+
+  users.forEach(function (user) {
+    buffer += user.name.full + '<br>'
+  })
+  res.send(buffer)
+})
+
+app.get('/yo', function (req, res) {
+  res.send('YO!')
+})
+
+var server = app.listen(3000, function() {
+  console.log('Server running at http://localhost:' + server.address().port)
+})
